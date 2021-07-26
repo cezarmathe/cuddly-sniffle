ac74eadc29684d24264db045c7ab3161e3b2be22
Armand-Cezar Mathe <me@cezarmathe.com>
2021-07-26 10:54:14 +0300

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 //! # qrwcell - quick read-write cell
 //!
-//! This is a cell with two slots - one for reading and one for writing. Writing
+//! Read-write cell that aims to reduce the amount of blocking compared to a
+//! single read-write lock.
+//!
+//! The cell has two slots - one for reading and one for writing. Writing
 //! alternates the slot that is currently served to readers, thereby minimising
 //! blocking on a reader-writer lock.
 //!
 //! Please be aware that if a cell is not created with a value or updated at
 //! least once attempting to get the inner value will loop forever!
+
 
 mod raw;
 
