ea7dd93cc13514c19d2a5836e9c07b92dd75db2d
Armand-Cezar Mathe <me@cezarmathe.com>
2021-07-29 09:40:11 +0300

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@
 //! Please be aware that if a cell is not created with a value or updated at
 //! least once attempting to get the inner value will loop forever!
 
-
 mod raw;
 
 use std::sync::Arc;
@@ -76,5 +75,94 @@ impl<T: Default> QrwCell<T> {
     #[inline(always)]
     pub fn update_with_default(&self) -> Arc<T> {
         self.update(T::default())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::QrwCell;
+
+    #[test]
+    #[should_panic]
+    fn test_new_get_panic() {
+        let cell: QrwCell<Vec<u32>> = QrwCell::new();
+        let _ = cell.get();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_new_get_weak_panic() {
+        let cell: QrwCell<Vec<u32>> = QrwCell::new();
+        let _ = cell.get_weak();
+    }
+
+    #[test]
+    fn test_default_get() {
+        let cell: QrwCell<Vec<u32>> = QrwCell::default();
+        assert_eq!(<Vec<u32>>::default(), *cell.get())
+    }
+
+    #[test]
+    fn test_default_get_weak() {
+        let cell: QrwCell<Vec<u32>> = QrwCell::default();
+        assert_eq!(
+            <Vec<u32>>::default(),
+            *cell
+                .get_weak()
+                .upgrade()
+                .expect("Weak pointer missing value")
+        )
+    }
+
+    #[test]
+    fn test_with_value_get() {
+        let cell = QrwCell::with_value(vec![42, 42, 42, 42]);
+        assert_eq!(vec![42, 42, 42, 42], *cell.get())
+    }
+
+    #[test]
+    fn test_with_value_get_weak() {
+        let cell: QrwCell<Vec<u32>> = QrwCell::with_value(vec![42, 42, 42, 42]);
+        assert_eq!(
+            vec![42, 42, 42, 42],
+            *cell
+                .get_weak()
+                .upgrade()
+                .expect("Weak pointer missing value")
+        )
+    }
+
+    #[test]
+    fn test_update() {
+        let cell = QrwCell::new();
+
+        let vec = vec![0, 0, 0, 0];
+        let vec_clone = vec.clone();
+        assert_eq!(*cell.update(vec), vec_clone);
+
+        let new_vec = vec![42, 0, 0, 0];
+        let new_vec_clone = new_vec.clone();
+        assert_eq!(*cell.update(new_vec), vec_clone);
+        assert_eq!(*cell.get(), new_vec_clone);
+    }
+
+    #[test]
+    fn test_update_default() {
+        let cell = QrwCell::new();
+
+        assert_eq!(*cell.update(vec![0, 0, 0, 0]), vec![0, 0, 0, 0]);
+
+        assert_eq!(*cell.update_with_default(), vec![0, 0, 0, 0]);
+        assert_eq!(*cell.get(), Vec::default());
+    }
+
+    #[test]
+    fn test_get_weak_update() {
+        let cell = QrwCell::with_value(vec![42, 42, 42, 42]);
+        assert_eq!(*cell.get_weak().upgrade().unwrap(), vec![42, 42, 42, 42]);
+
+        let weak = cell.get_weak();
+        cell.update_with_default();
+        assert_eq!(weak.upgrade(), None);
     }
 }
